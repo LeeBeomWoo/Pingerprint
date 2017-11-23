a2c5326b62b6a3fe3bc9971112ee410027d844f8
LeeBeomWoo <vvaeru6@gmail.com>
2017-11-23 17:48:20 +0900

    first

--- app/src/main/java/tax/bcns/pingerprinter/FingerprintHandler.java
+++ app/src/main/java/tax/bcns/pingerprinter/FingerprintHandler.java
@@ -18,6 +18,8 @@ public class FingerprintHandler extends FingerprintManager.AuthenticationCallbac
 
     // You should use the CancellationSignal method whenever your app can no longer process user input, for example when your app goes
     // into the background. If you don’t use this method, then other apps will be unable to access the touch sensor, including the lockscreen!//
+    //앱이 더 이상 사용자 입력을 처리 할 수 없을 때마다 (예 : 앱이 백그라운드로 들어갈 때) CancellationSignal 메서드를 사용해야합니다.
+    // 이 방법을 사용하지 않으면 다른 앱이 lockscreen을 포함하여 터치 센서에 액세스 할 수 없습니다!
 
     private CancellationSignal cancellationSignal;
     private Context context;
@@ -27,7 +29,7 @@ public class FingerprintHandler extends FingerprintManager.AuthenticationCallbac
     }
 
     //Implement the startAuth method, which is responsible for starting the fingerprint authentication process//
-
+    // 지문 인증 프로세스를 시작하는 startAuth 메서드를 구현합니다. //
     public void startAuth(FingerprintManager manager, FingerprintManager.CryptoObject cryptoObject) {
 
         cancellationSignal = new CancellationSignal();
@@ -37,37 +39,41 @@ public class FingerprintHandler extends FingerprintManager.AuthenticationCallbac
         manager.authenticate(cryptoObject, cancellationSignal, 0, this, null);
     }
 
-    @Override
     //onAuthenticationError is called when a fatal error has occurred. It provides the error code and error message as its parameters//
-
+    // onAuthenticationError는 치명적인 오류가 발생하면 호출됩니다. 오류 코드와 오류 메시지를 매개 변수로 제공합니다 //
+    @Override
     public void onAuthenticationError(int errMsgId, CharSequence errString) {
 
         //I’m going to display the results of fingerprint authentication as a series of toasts.
         //Here, I’m creating the message that’ll be displayed if an error occurs//
-
+        // 지문 인증 결과를 일련의 토스트로 표시 할 것입니다.
+        // 여기에 오류가 발생하면 표시 될 메시지가 생성됩니다. //
         Toast.makeText(context, "Authentication error\n" + errString, Toast.LENGTH_LONG).show();
     }
 
-    @Override
 
     //onAuthenticationFailed is called when the fingerprint doesn’t match with any of the fingerprints registered on the device//
-
+    // onAuthenticationFailed는 지문이 장치에 등록 된 지문 중 하나와 일치하지 않을 때 호출됩니다.
+    @Override
     public void onAuthenticationFailed() {
         Toast.makeText(context, "Authentication failed", Toast.LENGTH_LONG).show();
     }
 
-    @Override
 
     //onAuthenticationHelp is called when a non-fatal error has occurred. This method provides additional information about the error,
-    //so to provide the user with as much feedback as possible I’m incorporating this information into my toast//
+    //so to provide the user with as much feedback as possible I’m incorporating this information into my toast
+    // onAuthenticationHelp는 치명적이지 않은 오류가 발생하면 호출됩니다. 이 메서드는 오류에 대한 추가 정보를 제공합니다.
+    // 가능한 한 많은 피드백을 사용자에게 제공합니다.이 정보를 내 토스트에 통합합니다. //
+    @Override
     public void onAuthenticationHelp(int helpMsgId, CharSequence helpString) {
         Toast.makeText(context, "Authentication help\n" + helpString, Toast.LENGTH_LONG).show();
-    }@Override
-
-    //onAuthenticationSucceeded is called when a fingerprint has been successfully matched to one of the fingerprints stored on the user’s device//
+    }
+    //onAuthenticationSucceeded is called when a fingerprint has been successfully matched to one of the fingerprints stored on the user’s device
+    //onAuthenticationSucceeded는 사용자의 기기에 저장된 지문 중 하나와 지문이 성공적으로 일치하면 호출됩니다
+    @Override
     public void onAuthenticationSucceeded(
             FingerprintManager.AuthenticationResult result) {
-
+        result.getCryptoObject();
         Toast.makeText(context, "Success!", Toast.LENGTH_LONG).show();
     }
 
--- app/src/main/java/tax/bcns/pingerprinter/MainActivity.java
+++ app/src/main/java/tax/bcns/pingerprinter/MainActivity.java
@@ -35,6 +35,7 @@ import javax.crypto.SecretKey;
 public class MainActivity extends AppCompatActivity {
 
     // Declare a string variable for the key we’re going to use in our fingerprint authentication
+    // 지문 인증에 사용할 키의 문자열 변수를 선언합니다.
     private static final String KEY_NAME = "yourKey";
     private Cipher cipher;
     private KeyStore keyStore;
@@ -51,6 +52,7 @@ public class MainActivity extends AppCompatActivity {
 
         // If you’ve set your app’s minSdkVersion to anything lower than 23, then you’ll need to verify that the device is running Marshmallow
         // or higher before executing any fingerprint-related code
+        // 앱의 minSdkVersion을 23 미만으로 설정 한 경우 지문 관련 코드를 실행하기 전에 기기가 마시맬로 이상을 실행하는지 확인해야합니다
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
             //Get an instance of KeyguardManager and FingerprintManager//
             keyguardManager =
@@ -61,25 +63,32 @@ public class MainActivity extends AppCompatActivity {
             textView = (TextView) findViewById(R.id.textview);
 
             //Check whether the device has a fingerprint sensor//
+            // 장치에 지문 센서가 있는지 확인합니다 //
             if (!fingerprintManager.isHardwareDetected()) {
                 // If a fingerprint sensor isn’t available, then inform the user that they’ll be unable to use your app’s fingerprint functionality//
+                // 지문 센서를 사용할 수없는 경우 사용자에게 앱의 지문 인식 기능을 사용할 수 없음을 알립니다. //
                 textView.setText("Your device doesn't support fingerprint authentication");
             }
             //Check whether the user has granted your app the USE_FINGERPRINT permission//
+            //사용자가 앱에 USE_FINGERPRINT 권한을 부여했는지 확인합니다. //
             if (ActivityCompat.checkSelfPermission(this, Manifest.permission.USE_FINGERPRINT) != PackageManager.PERMISSION_GRANTED) {
                 // If your app doesn't have this permission, then display the following text//
                 textView.setText("Please enable the fingerprint permission");
             }
 
             //Check that the user has registered at least one fingerprint//
+            // 사용자가 적어도 하나의 지문을 등록했는지 확인합니다 //
             if (!fingerprintManager.hasEnrolledFingerprints()) {
                 // If the user hasn’t configured any fingerprints, then display the following message//
+                // 사용자가 지문을 구성하지 않은 경우 다음 메시지를 표시합니다. //
                 textView.setText("No fingerprint configured. Please register at least one fingerprint in your device's Settings");
             }
 
             //Check that the lockscreen is secured//
+            // 잠금 화면이 안전한지 확인합니다 //
             if (!keyguardManager.isKeyguardSecure()) {
                 // If the user hasn’t secured their lockscreen with a PIN password or pattern, then display the following text//
+                // 사용자가 PIN 비밀번호 나 패턴으로 잠금 화면을 보호하지 않은 경우 다음 텍스트를 표시합니다. //
                 textView.setText("Please enable lockscreen security in your device's Settings");
             } else {
                 try {        generateKey();
@@ -89,10 +98,13 @@ public class MainActivity extends AppCompatActivity {
 
                 if (initCipher()) {
                     //If the cipher is initialized successfully, then create a CryptoObject instance//
+                    // 암호가 성공적으로 초기화 된 경우 CryptoObject 인스턴스를 만듭니다. //
                     cryptoObject = new FingerprintManager.CryptoObject(cipher);
 
                     // Here, I’m referencing the FingerprintHandler class that we’ll create in the next section. This class will be responsible
                     // for starting the authentication process (via the startAuth method) and processing the authentication process events//
+                    // 여기에서는 다음 섹션에서 만들 FingerprintHandler 클래스를 참조합니다.
+                    // 이 클래스는 (startAuth 메소드를 통해) 인증 프로세스를 시작하고 인증 프로세스 이벤트를 처리합니다.
                     FingerprintHandler helper = new FingerprintHandler(this);
                     helper.startAuth(fingerprintManager, cryptoObject);
                 }
@@ -101,10 +113,11 @@ public class MainActivity extends AppCompatActivity {
     }
 
 //Create the generateKey method that we’ll use to gain access to the Android keystore and generate the encryption key//
-
+// Android 키 저장소에 액세스하고 암호화 키를 생성하는 데 사용할 generateKey 메서드를 만듭니다. //
     private void generateKey() throws FingerprintException {
         try {
             // Obtain a reference to the Keystore using the standard Android keystore container identifier (“AndroidKeystore”)//
+            // 표준 Android 키 저장소 컨테이너 식별자 ( "AndroidKeystore")를 사용하여 키 저장소에 대한 참조 얻기 //
             keyStore = KeyStore.getInstance("AndroidKeyStore");
 
             //Generate the key//
@@ -117,12 +130,14 @@ public class MainActivity extends AppCompatActivity {
             keyGenerator.init(new
 
                     //Specify the operation(s) this key can be used for//
+                    // 이 키를 사용할 수있는 작업 지정 //
                     KeyGenParameterSpec.Builder(KEY_NAME,
                     KeyProperties.PURPOSE_ENCRYPT |
                             KeyProperties.PURPOSE_DECRYPT)
                     .setBlockModes(KeyProperties.BLOCK_MODE_CBC)
 
                     //Configure this key so that the user has to confirm their identity with a fingerprint each time they want to use it//
+                    //이 키를 구성하여 사용자가 사용할 때마다 지문을 사용하여 ID를 확인해야합니다.
                     .setUserAuthenticationRequired(true)
                     .setEncryptionPaddings(
                             KeyProperties.ENCRYPTION_PADDING_PKCS7)
@@ -143,9 +158,11 @@ public class MainActivity extends AppCompatActivity {
     }
 
     //Create a new method that we’ll use to initialize our cipher//
+    // 암호를 초기화하는 데 사용할 새 메서드를 만듭니다.
     public boolean initCipher() {
         try {
             //Obtain a cipher instance and configure it with the properties required for fingerprint authentication//
+            // 암호 인스턴스를 얻어 지문 인증에 필요한 속성으로 구성합니다. //
             cipher = Cipher.getInstance(
                     KeyProperties.KEY_ALGORITHM_AES + "/"
                             + KeyProperties.BLOCK_MODE_CBC + "/"
@@ -161,6 +178,7 @@ public class MainActivity extends AppCompatActivity {
                     null);
             cipher.init(Cipher.ENCRYPT_MODE, key);
             //Return true if the cipher has been initialized successfully//
+            // 암호가 성공적으로 초기화 된 경우 true를 반환합니다.
             return true;
         } catch (KeyPermanentlyInvalidatedException e) {
 
